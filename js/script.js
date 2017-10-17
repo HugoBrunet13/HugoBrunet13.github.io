9d5b19e2fa5afecf32499897aba4327c181d5b84
Hugo <h.brunet13@gmail.com>
2017-10-17 12:53:34 +0200

    hu

--- js/script.js
+++ js/script.js
@@ -3,23 +3,27 @@ $(document).ready(function() {
 	$(".input-group-addon").click(function(){
 		console.log("ntm");
 		console.log($(".form-control").text());
-	});
 
-	$.ajax({
-		url : "https://bitcoin.mubiz.com/block/"+$(".form-control").text()+"/",
-		dataType : "json",
-		contentType : "application/json; charset=utf-8",
-		type : "GET",
-		timeout:	"5000",
-		async : false,
 
-		success : function(data) {
-			console.log(data);
-			$('#ReponseWS').append(data.size);
-		},
 
-		error : function(xhr, status, err) {
-			$('#ReponseWS').append(err+" N/A");
-		}
+
+
+		$.ajax({
+			url : "https://bitcoin.mubiz.com/block/"+$(".form-control").text()+"/",
+			dataType : "json",
+			contentType : "application/json; charset=utf-8",
+			type : "GET",
+			timeout:	"5000",
+			async : false,
+
+			success : function(data) {
+				console.log(data);
+				$('#ReponseWS').append(data.size);
+			},
+
+			error : function(xhr, status, err) {
+				$('#ReponseWS').append(err+" N/A");
+			}
+		});
 	});
 });
