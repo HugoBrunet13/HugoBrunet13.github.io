c7ec95fad68b838a14a85475ba31b3fb8e3ba21f
Hugo <h.brunet13@gmail.com>
2017-10-17 23:13:09 +0200

    hugo

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
 	}
 
  	function RechercherTransaction(idTransaction,urlWS){
- 		$("#numTransac")"";
+ 		$("#numTransac")="";
  	// 	var retourAjax = AppelWS(idTransaction,urlWS,"#infoTranscation");
 		// var data = retourAjax.responseJSON;
 		// console.log(data);
@@ -49,16 +49,19 @@ $(document).ready(function() {
 	}
 
 	function MiseEnFormeInfoBlock(data){
+		//supprimer contenu de la div a chaque nouvel appel
 		$("#infoBlock").show();
 		$("#infoBlock").append("infoblock1;infoblock2,info3block....");
 	}
 
 	function MiseEnFormeInfoTransaction(data){
+		//supprimer contenu de la div a chaque nouvel appel
 		$("#infoTransaction").show();
 		$("#infoTransaction").append("infotransac1;transac2....");
 	}
 
 	function MiseEnFormeInfoAdresse(data){
+		//supprimer contenu de la div a chaque nouvel appel
 		$("#infoAdresse").show();
 		$("#infoAdresse").append("infoAdresse;infoAdresse2....");
 	}
