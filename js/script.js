e51da2e8bc436e7760178fda75384b487992275c
HugoBrunet13 <h.brunet13@gmail.com>
2017-12-11 01:16:13 +0100

    Hugo

--- js/script.js
+++ js/script.js
@@ -166,7 +166,7 @@ $(document).ready(function() {
 			for (var i = 0; i < 5; i++) {
 				$("#ADR_tx"+i).append(data.txrefs[i]["TX_hash"]);
 				cpt = cpt+1;
-				if (!data.txs[i+1])
+				if (!data.txrefs[i+1])
 					break;
 			}
 		}
