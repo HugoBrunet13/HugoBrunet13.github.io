68b7aa66e4cea3e4a57f64d3e92785128cc95395
HugoBrunet13 <h.brunet13@gmail.com>
2017-12-25 12:30:14 +0100

    hUGO

--- bitcoin/script.js
+++ bitcoin/script.js
@@ -2,212 +2,212 @@ $(document).ready(function() {
 
 	$("#BoutonRechercheBLock").click(function(){
 		if($("#Form_idBlock").val()!=""){
-			RechercherBlock($("#Form_idBlock").val(),"https://bitcoin.mubiz.com/block/");
+			window.location.href="block.html";
 		}
 	});
 
-	function RechercherBlock(indexBLock,urlWS){
-		$("#Form_idBlock").val('');
-		window.location.href="block.html";
-		console.log("HUgo");
-		var retourAjax = AppelWS(indexBLock,urlWS,"#erreurblock");
-		console.log("HUgo");
-		var data = retourAjax.responseJSON;
-		MiseEnFormeInfoBlock(data);
-	}
+	// function RechercherBlock(indexBLock,urlWS){
+	// 	$("#Form_idBlock").val('');
+	// 	window.location.href="block.html";
+	// 	console.log("HUgo");
+	// 	var retourAjax = AppelWS(indexBLock,urlWS,"#erreurblock");
+	// 	console.log("HUgo");
+	// 	var data = retourAjax.responseJSON;
+	// 	MiseEnFormeInfoBlock(data);
+	// }
 
-	function RechercherBlockParHash(hashBlock,urlWS){
-		var retourAjax = AppelWS(hashBlock,urlWS,"#erreurblock");
-		var data = retourAjax.responseJSON;
-		RechercherBlock(data.height,"https://bitcoin.mubiz.com/block/");
-	}
+	// function RechercherBlockParHash(hashBlock,urlWS){
+	// 	var retourAjax = AppelWS(hashBlock,urlWS,"#erreurblock");
+	// 	var data = retourAjax.responseJSON;
+	// 	RechercherBlock(data.height,"https://bitcoin.mubiz.com/block/");
+	// }
 
- 	function RechercherTransaction(idTransaction,urlWS){
- 		$("#numTransac").val('');
- 	 	var retourAjax = AppelWS2(idTransaction,urlWS,"#erreurTransac");
-		var data = retourAjax.responseJSON;
-		MiseEnFormeInfoTransaction(data);
- 	}
+ // 	function RechercherTransaction(idTransaction,urlWS){
+ // 		$("#numTransac").val('');
+ // 	 	var retourAjax = AppelWS2(idTransaction,urlWS,"#erreurTransac");
+	// 	var data = retourAjax.responseJSON;
+	// 	MiseEnFormeInfoTransaction(data);
+ // 	}
  	
-	function RechercherAdresse(adresse,urlWS){
- 		$("#adresse").val('');
- 	 	var retourAjax = AppelWS2(adresse,urlWS,"#erreurAdresse");
-		var data = retourAjax.responseJSON;
-		MiseEnFormeInfoAdresse(data);
-	}
+	// function RechercherAdresse(adresse,urlWS){
+ // 		$("#adresse").val('');
+ // 	 	var retourAjax = AppelWS2(adresse,urlWS,"#erreurAdresse");
+	// 	var data = retourAjax.responseJSON;
+	// 	MiseEnFormeInfoAdresse(data);
+	// }
 
-	function AppelWS(param,urlWS,divErreur){
-		var appelAjax = $.ajax({
-			url : urlWS+param+"/",
-			dataType : "json",
-			contentType : "application/json; charset=utf-8",
-			type : "GET",
-			timeout:"5000",
-			async : false,
-			success : function(data) {
+	// function AppelWS(param,urlWS,divErreur){
+	// 	var appelAjax = $.ajax({
+	// 		url : urlWS+param+"/",
+	// 		dataType : "json",
+	// 		contentType : "application/json; charset=utf-8",
+	// 		type : "GET",
+	// 		timeout:"5000",
+	// 		async : false,
+	// 		success : function(data) {
 				
-			},
-			error : function(xhr, status, err) {
-				$(divErreur).text('');
-				$(divErreur).append(err);
-			}
-		});
-		return appelAjax;
-	}
+	// 		},
+	// 		error : function(xhr, status, err) {
+	// 			$(divErreur).text('');
+	// 			$(divErreur).append(err);
+	// 		}
+	// 	});
+	// 	return appelAjax;
+	// }
 
 
-	function AppelWS2(param,urlWS,divErreur){
-		var appelAjax = $.ajax({
-			url : urlWS+param,
-			dataType : "json",
-			contentType : "application/json; charset=utf-8",
-			type : "GET",
-			timeout:"5000",
-			async : false,
-			success : function(data) {
+	// function AppelWS2(param,urlWS,divErreur){
+	// 	var appelAjax = $.ajax({
+	// 		url : urlWS+param,
+	// 		dataType : "json",
+	// 		contentType : "application/json; charset=utf-8",
+	// 		type : "GET",
+	// 		timeout:"5000",
+	// 		async : false,
+	// 		success : function(data) {
 				
-			},
-			error : function(xhr, status, err) {
-				$(divErreur).text('');
-			}
-		});
-		return appelAjax;
-	}
+	// 		},
+	// 		error : function(xhr, status, err) {
+	// 			$(divErreur).text('');
+	// 		}
+	// 	});
+	// 	return appelAjax;
+	// }
 
-	function ClearResultatBlock(){
-		$("#BLOCK_hash").text('');
-		$("#BLOCK_confirmation").text('');
-		$("#BLOCK_size").text('');
-		$("#BLOCK_weight").text('');
-		$("#BLOCK_height").text('');
-		$("#BLOCK_version").text('');
-		$("#BLOCK_previousBlockHash").text('');
-		$("#BLOCK_nextBlockHash").text('');
-		$(".BLOCK_tx").text('');
-	}
+	// function ClearResultatBlock(){
+	// 	$("#BLOCK_hash").text('');
+	// 	$("#BLOCK_confirmation").text('');
+	// 	$("#BLOCK_size").text('');
+	// 	$("#BLOCK_weight").text('');
+	// 	$("#BLOCK_height").text('');
+	// 	$("#BLOCK_version").text('');
+	// 	$("#BLOCK_previousBlockHash").text('');
+	// 	$("#BLOCK_nextBlockHash").text('');
+	// 	$(".BLOCK_tx").text('');
+	// }
 
-	function ClearResultatTransaction(){
-		$("#TX_hash").text('');
-		$("#TX_size").text('');
-		$("#TX_blockheight").text('');
-		$("#TX_confirmations").text('');
-		$("#TX_adr0").text('');
-		$("#TX_adr1").text('');
-	}
+	// function ClearResultatTransaction(){
+	// 	$("#TX_hash").text('');
+	// 	$("#TX_size").text('');
+	// 	$("#TX_blockheight").text('');
+	// 	$("#TX_confirmations").text('');
+	// 	$("#TX_adr0").text('');
+	// 	$("#TX_adr1").text('');
+	// }
 
-	function ClearResultatAdresse(){
-		$("#ADR_adresse").text('');
-		$("#ADR_n_tx").text('');
-		$("#ADR_total_received").text('');
-		$("#ADR_total_sent").text('');
-		$("#ADR_final_balance").text('');
-		$(".ADR_tx").text('');
-	}
+	// function ClearResultatAdresse(){
+	// 	$("#ADR_adresse").text('');
+	// 	$("#ADR_n_tx").text('');
+	// 	$("#ADR_total_received").text('');
+	// 	$("#ADR_total_sent").text('');
+	// 	$("#ADR_final_balance").text('');
+	// 	$(".ADR_tx").text('');
+	// }
 
-	function MiseEnFormeInfoBlock(data){
-		$("#infoBlock").show();
-		$("#tableBlock").show();
-		$("#erreurblock").show();
-		if (data){
-			$("#erreurblock").hide();
-			ClearResultatBlock();
-			$("#BLOCK_hash").append(data.hash);
-			$("#BLOCK_confirmation").append(data.confirmations);
-			$("#BLOCK_size").append(data.size);
-			$("#BLOCK_weight").append(data.weight);
-			$("#BLOCK_height").append(data.height);
-			$("#BLOCK_version").append(data.version);
-			$("#BLOCK_previousBlockHash").append(data.previousblockhash);
-			$("#BLOCK_nextBlockHash").append(data.nextblockhash);
-			//var cpt = 0;
-			console.log(data.tx.length); 
-			for (var i = 0; i < data.tx.length; i++) {
-				$("#listOfTx").append('<a href="javascript:testblocktx()"><div class="BLOCK_tx">'+data.tx[i]+'</div></a>');
-				// cpt = cpt+1;
-				// if (!data.tx[i+1])
-				// 	break;
-			}
-		} else{
-			$("#tableBlock").hide();
-		}
-	}
+	// function MiseEnFormeInfoBlock(data){
+	// 	$("#infoBlock").show();
+	// 	$("#tableBlock").show();
+	// 	$("#erreurblock").show();
+	// 	if (data){
+	// 		$("#erreurblock").hide();
+	// 		ClearResultatBlock();
+	// 		$("#BLOCK_hash").append(data.hash);
+	// 		$("#BLOCK_confirmation").append(data.confirmations);
+	// 		$("#BLOCK_size").append(data.size);
+	// 		$("#BLOCK_weight").append(data.weight);
+	// 		$("#BLOCK_height").append(data.height);
+	// 		$("#BLOCK_version").append(data.version);
+	// 		$("#BLOCK_previousBlockHash").append(data.previousblockhash);
+	// 		$("#BLOCK_nextBlockHash").append(data.nextblockhash);
+	// 		//var cpt = 0;
+	// 		console.log(data.tx.length); 
+	// 		for (var i = 0; i < data.tx.length; i++) {
+	// 			$("#listOfTx").append('<a href="javascript:testblocktx()"><div class="BLOCK_tx">'+data.tx[i]+'</div></a>');
+	// 			// cpt = cpt+1;
+	// 			// if (!data.tx[i+1])
+	// 			// 	break;
+	// 		}
+	// 	} else{
+	// 		$("#tableBlock").hide();
+	// 	}
+	// }
 
-	function MiseEnFormeInfoTransaction(data){
-		$("#infoTransaction").show();
-		$("#tabletransac").show();
-		$("#erreurTransac").show();
-		if (!data.error && data){
-			$("#erreurTransac").hide();
-			ClearResultatTransaction();
-			$("#TX_hash").append(data.hash);
-			$("#TX_size").append(data.size);
-			$("#TX_blockheight").append(data.block_height);
-			$("#TX_confirmations").append(data.confirmations);
-			$("#TX_adr0").append(data.addresses[0]);
-			$("#TX_adr1").append(data.addresses[1]);
-		} else{
-			$("#erreurTransac").append(data.error);
-			$("#tabletransac").hide();
-		}
-	}
+	// function MiseEnFormeInfoTransaction(data){
+	// 	$("#infoTransaction").show();
+	// 	$("#tabletransac").show();
+	// 	$("#erreurTransac").show();
+	// 	if (!data.error && data){
+	// 		$("#erreurTransac").hide();
+	// 		ClearResultatTransaction();
+	// 		$("#TX_hash").append(data.hash);
+	// 		$("#TX_size").append(data.size);
+	// 		$("#TX_blockheight").append(data.block_height);
+	// 		$("#TX_confirmations").append(data.confirmations);
+	// 		$("#TX_adr0").append(data.addresses[0]);
+	// 		$("#TX_adr1").append(data.addresses[1]);
+	// 	} else{
+	// 		$("#erreurTransac").append(data.error);
+	// 		$("#tabletransac").hide();
+	// 	}
+	// }
 
-	function MiseEnFormeInfoAdresse(data){
-		$("#infoAdresse").show();
-		$("#tableadresse").show();
-		if(data.error){
-			$("#erreurAdresse").show();
-			$("#erreurAdresse").append(data.error);
-			$("#infoAdresse").show();
-			$("#tableadresse").hide();
-		}
-		else if(data){
-			$("#erreurAdresse").hide();
-			ClearResultatAdresse();
-			$("#ADR_adresse").append(data.address);
-			$("#ADR_hash").append(data.hash160);
-			$("#ADR_n_tx").append(data.n_tx);
-			$("#ADR_total_received").append(data.total_received);
-			$("#ADR_total_sent").append(data.total_sent);
-			$("#ADR_final_balance").append(data.final_balance);
-			var cpt = 0;
-			for (var i = 0; i < 5; i++) {
-				$("#ADR_tx"+i).append(data.txrefs[i]["tx_hash"]);
-				cpt = cpt+1;
-				if (!data.txrefs[i+1])
-					break;
-			}
-		}
-	}
+	// function MiseEnFormeInfoAdresse(data){
+	// 	$("#infoAdresse").show();
+	// 	$("#tableadresse").show();
+	// 	if(data.error){
+	// 		$("#erreurAdresse").show();
+	// 		$("#erreurAdresse").append(data.error);
+	// 		$("#infoAdresse").show();
+	// 		$("#tableadresse").hide();
+	// 	}
+	// 	else if(data){
+	// 		$("#erreurAdresse").hide();
+	// 		ClearResultatAdresse();
+	// 		$("#ADR_adresse").append(data.address);
+	// 		$("#ADR_hash").append(data.hash160);
+	// 		$("#ADR_n_tx").append(data.n_tx);
+	// 		$("#ADR_total_received").append(data.total_received);
+	// 		$("#ADR_total_sent").append(data.total_sent);
+	// 		$("#ADR_final_balance").append(data.final_balance);
+	// 		var cpt = 0;
+	// 		for (var i = 0; i < 5; i++) {
+	// 			$("#ADR_tx"+i).append(data.txrefs[i]["tx_hash"]);
+	// 			cpt = cpt+1;
+	// 			if (!data.txrefs[i+1])
+	// 				break;
+	// 		}
+	// 	}
+	// }
 
-	function testblocktx(){
-		console.log("youououououou");
-	}
+	// function testblocktx(){
+	// 	console.log("youououououou");
+	// }
 	
 
-	//----------------------------------------------------------------------------
-	//CacherDivInfo();	
+	// //----------------------------------------------------------------------------
+	// //CacherDivInfo();	
 
 
-	$("#boutonRecherche").click(function(){
-		if ($("#numBlock").val()!=""){
-			//CacherDivInfo();
-			// window.open("block.html");
-			//load("block.html");
-			RechercherBlock($("#numBlock").val(),"https://bitcoin.mubiz.com/block/");
-		} 
-		if($("#numTransac").val()!=""){
-			//CacherDivInfo();
-			window.open("transaction.html");
-			// loadPage("transaction.html")
-			// RechercherTransaction($("#numTransac").val(),"https://api.blockcypher.com/v1/btc/main/txs/");
-		} 
-		if ($("#adresse").val()!=""){
-			window.open("adresse.html");
-			// CacherDivInfo();
-			// loadPage("adresse.html");
-			// RechercherAdresse($("#adresse").val(),"https://api.blockcypher.com/v1/btc/main/addrs/");
-		}
-	});
+	// $("#boutonRecherche").click(function(){
+	// 	if ($("#numBlock").val()!=""){
+	// 		//CacherDivInfo();
+	// 		// window.open("block.html");
+	// 		//load("block.html");
+	// 		RechercherBlock($("#numBlock").val(),"https://bitcoin.mubiz.com/block/");
+	// 	} 
+	// 	if($("#numTransac").val()!=""){
+	// 		//CacherDivInfo();
+	// 		window.open("transaction.html");
+	// 		// loadPage("transaction.html")
+	// 		// RechercherTransaction($("#numTransac").val(),"https://api.blockcypher.com/v1/btc/main/txs/");
+	// 	} 
+	// 	if ($("#adresse").val()!=""){
+	// 		window.open("adresse.html");
+	// 		// CacherDivInfo();
+	// 		// loadPage("adresse.html");
+	// 		// RechercherAdresse($("#adresse").val(),"https://api.blockcypher.com/v1/btc/main/addrs/");
+	// 	}
+	// });
 
 
 	// $("#TX_blockheight").click(function(){
@@ -318,9 +318,9 @@ $(document).ready(function() {
 
 
 
-	function test(){
-		console.log("testmaggl");
-	}
+	// function test(){
+	// 	console.log("testmaggl");
+	// }
 
 
 });	
