d00e1b4022d1c8635a38195fe56b422212f4a58b
HugoBrunet13 <h.brunet13@gmail.com>
2017-12-26 10:08:10 +0100

    Hugo

--- bitcoin/script.js
+++ bitcoin/script.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
 	$("#BoutonRechercheBLock").click(function(){
 		if($("#Form_idBlock").val()!=""){
-			window.location.href="block.html"+"?id="+$("#Form_idBlock").text();
+			window.location.href="block.html"+"?idBlock="+ $(".Form_idBlock").text();
 		}
 	});
 
