c158e4a91384c70999f8e72a83b5482bbb44efe8
HugoBrunet13 <h.brunet13@gmail.com>
2017-12-26 11:29:35 +0100

    Hugo

--- bitcoin/scriptBlock.js
+++ bitcoin/scriptBlock.js
@@ -59,7 +59,7 @@ $(document).ready(function() {
 			for (var i = 0; i < data.tx.length; i++) {
 				$("#listOfTx").append('<a href="javascript:testblocktx()"><div class="BLOCK_tx">'+data.tx[i]+'</div></a>');
 			}
-		} else if (typeof data === 'undifined'){
+		} else if (typeof data === 'undefined'){
 			$("#errorBlock").append("Error: block not find").show()
 		}else{
 			$("#errorBlock").append(data.error).show()	;
