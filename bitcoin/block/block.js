71547168c3e6c9343ded09b1b2b8410537641d3a
HugoBrunet13 <h.brunet13@gmail.com>
2018-04-01 21:02:53 +0200

    maintenance

--- bitcoin/block/block.js
+++ bitcoin/block/block.js
@@ -1,26 +1,26 @@
 $(document).ready(function() {
 	function BlockIdResearch(indexBLock,urlWS){
 		$("#idBlock").val('');
-		var retourAjax = AppelWS(indexBLock,urlWS);
+		var retourAjax = AppelWS2(indexBLock,urlWS);
 		var data = retourAjax.responseJSON;
 		MiseEnFormeInfoBlock(data);
 	}
 
 	function BlockHashResearch(hashBlock,urlWS){
-		var retourAjax = AppelWS(hashBlock,urlWS);
+		var retourAjax = AppelWS2(hashBlock,urlWS);
 		var data = retourAjax.responseJSON;
-		BlockIdResearch(data.height,"https://bitcoin.mubiz.com/block/");
+		BlockIdResearch(data.height,"https://api.blockcypher.com/v1/btc/main/blocks/");
 	}
 
 	function ClearResultatBlock(){
 		$("#BLOCK_hash").text('');
-		$("#BLOCK_confirmation").text('');
+		$("#BLOCK_time").text('');
+		$("#BLOCK_receivedTime").text('');
+		$("#BLOCK_chain").text('');
 		$("#BLOCK_size").text('');
-		$("#BLOCK_weight").text('');
+		$("#BLOCK_total").text('');
 		$("#BLOCK_height").text('');
-		$("#BLOCK_version").text('');
 		$("#BLOCK_previousBlockHash").text('');
-		$("#BLOCK_nextBlockHash").text('');
 		$("#listOfTx").text('');
 	}
 
@@ -29,13 +29,13 @@ $(document).ready(function() {
 			document.getElementById('tableBlock').style.visibility = 'visible';
 			ClearResultatBlock();
 			$("#BLOCK_hash").append(data.hash);
-			$("#BLOCK_confirmation").append(data.confirmations);
+			$("#BLOCK_time").append(data.time);
+			$("#BLOCK_receivedTime").append(data.received_time);
+			$("#BLOCK_chain").append(data.chain);
 			$("#BLOCK_size").append(data.size);
-			$("#BLOCK_weight").append(data.weight);
+			$("#BLOCK_total").append(data.total);
 			$("#BLOCK_height").append(data.height);
-			$("#BLOCK_version").append(data.version);
-			$("#BLOCK_previousBlockHash").append('<a href="block.html?hashBlock='+data.previousblockhash+'">'+data.previousblockhash+'</a>');
-			$("#BLOCK_nextBlockHash").append('<a href="block.html?hashBlock='+data.nextblockhash+'">'+data.nextblockhash+'</a>');
+			$("#BLOCK_previousBlockHash").append('<a href="block.html?hashBlock='+data.prev_block+'">'+data.prev_block+'</a>');
 			for (var i = 0; i < data.tx.length; i++) {
 				$("#listOfTx").append('<a href="../transaction/transaction.html?hashTx='+data.tx[i]+'">'+data.tx[i]+'</a><br>');
 			}
@@ -45,13 +45,13 @@ $(document).ready(function() {
 		}
 	}
 
-	//----------------------------------------------------------------------------
-
+	//-------------------------------------------------------------------------------------------------------
+ 
 	if (GetURLParameter('idBlock')){
-		BlockIdResearch(GetURLParameter('idBlock'),"https://bitcoin.mubiz.com/block/");
+		BlockIdResearch(GetURLParameter('idBlock'),"https://api.blockcypher.com/v1/btc/main/blocks/");
 	}
 	else if(GetURLParameter('hashBlock')){
-		BlockHashResearch(GetURLParameter('hashBlock'),"https://bitcoin.mubiz.com/block_hash/");
+		BlockHashResearch(GetURLParameter('hashBlock'),"https://api.blockcypher.com/v1/btc/main/blocks/");
 	}
 
 });	
